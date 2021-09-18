98be11eef24187a1567d7bae5fcd09de58c1df0b
Floyd Fernandes <floydfernandes20@gmail.com>
2021-09-18 11:25:00 +0530

    Comment unsued code

--- src/components/ValuePrediction/HeartDiseasePrediction.js
+++ src/components/ValuePrediction/HeartDiseasePrediction.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 
 function HeartDiseasePrediction() {
-  const fetchResult = async (url, data) => {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    return response.json();
-  };
+  // const fetchResult = async (url, data) => {
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(data),
+  //   });
+  //   return response.json();
+  // };
   // const result = async () => {
   //   const response = await fetchResult("http://localhost:5000/predictapi", {
   //     data: [50, 0, 1, 200, 180, 180, 150, 180, 0, 2.5, 2, 1, 2],
