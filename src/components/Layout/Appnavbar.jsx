0e7da2266d2b283b05bf8188f61bda4ad47d3c96
Floyd Fernandes <floydfernandes20@gmail.com>
2021-10-07 11:02:41 +0530

    Implemented auth Workflow.

--- src/components/Layout/Appnavbar.jsx
+++ src/components/Layout/Appnavbar.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Nav, NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
 import LogoImage from "../../assets/images/surfboard.png";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import { useDispatch } from "react-redux";
 import "./styles.scss";
 
 function Appnavbar() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const location = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setScreenHeight();
+    window.addEventListener("resize", setScreenHeight);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const setScreenHeight = () => {
+    setScreenWidth(window.innerWidth);
+    console.log(screenWidth);
+  };
+
   const hideDropdown = () => {
     var dropDownMenu = document.getElementsByClassName("dropdown-menu show");
     var navItem = document.getElementById("basic-nav-dropdown");
@@ -22,6 +40,16 @@ function Appnavbar() {
       document.getElementById("navbar-toggle-button").click();
     }
   };
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+
+  const logout = () => {
+    hideNavbar();
+    dispatch({ type: "LOGOUT" });
+    history.push("/");
+    setUser(null);
+  };
 
   return (
     <Navbar expand="lg">
@@ -35,7 +63,21 @@ function Appnavbar() {
         <Navbar.Toggle
           id="navbar-toggle-button"
           aria-controls="basic-navbar-nav"
-        />
+        >
+          {user && screenWidth < 768 && (
+            <Avatar
+              className="profile-avatar mx-2"
+              alt={user?.result?.firstName[0]}
+              src={
+                user.result?.imageUrl
+                  ? user.result?.imageUrl
+                  : user.result?.firstName
+              }
+            />
+          )}
+          {screenWidth < 768 && <span className="navbar-toggler-icon"></span>}
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="PREDICTION" id="basic-nav-dropdown">
@@ -68,14 +110,31 @@ function Appnavbar() {
               ABOUT
             </Link>
           </Nav>
-          <Nav>
-            <Link
-              onClick={hideNavbar}
-              className="nav-link nav-link-login"
-              to="/login"
-            >
-              LOGIN
-            </Link>
+          <Nav className="profile-login-div">
+            {user && screenWidth > 768 && (
+              <Avatar
+                className="profile-avatar"
+                alt={user?.result?.firstName[0]}
+                src={
+                  user.result?.imageUrl
+                    ? user.result?.imageUrl
+                    : user.result?.firstName
+                }
+              />
+            )}
+            {user ? (
+              <Link onClick={logout} to="/" className="nav-link nav-link-login">
+                LOGOUT
+              </Link>
+            ) : (
+              <Link
+                onClick={hideNavbar}
+                className="nav-link nav-link-login"
+                to="/login"
+              >
+                LOGIN
+              </Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
