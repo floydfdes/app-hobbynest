949603c651291483a9c0a4efd9070e01cf944dd0
floydfdes <floydfernandes20@gmail.com>
2023-05-10 09:56:05 +0530

    New api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://postpred.herokuapp.com" });
-//const API = axios.create({ baseURL: "http://localhost:5000" });
+//const API = axios.create({ baseURL: "https://postpred.herokuapp.com" });
+const API = axios.create({ baseURL: "http://3.110.220.229:8000" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
