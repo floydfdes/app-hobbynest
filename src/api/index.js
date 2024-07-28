204484f0c5619c277a757c435fdc753f22190b1a
floydfdes <floydfernandes20@gmail.com>
2024-07-28 20:32:38 +0530

    New Api end url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const API = axios.create({ baseURL: "https://postpred.herokuapp.com" });
-const API = axios.create({ baseURL: "https://hobbynest-api.azurewebsites.net/" });
+const API = axios.create({ baseURL: "https://app-postpred-api-express.onrender.com/" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
