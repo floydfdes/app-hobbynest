b4c34f95a9f0769557cae21e79dfd9d8225a4e49
floydfdes <floydfernandes20@gmail.com>
2024-11-02 16:38:55 +0530

    Linting error fix

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -1,23 +1,34 @@
 import * as api from '../api/index';
 import { FETCH_COMMENTS, FETCH_POSTS, FETCH_USERS } from '../constants/actionTypes';
+import { notifyError } from './toastNotifications';
+
+const handleError = (dispatch, error) => {
+    dispatch(notifyError({ message: error.message, color: 'error' }));
+};
 
 export const fetchUsers = () => async (dispatch) => {
     try {
         const { data } = await api.fetchUsers();
         dispatch({ type: FETCH_USERS, payload: data });
-    } catch (error) { }
+    } catch (error) {
+        handleError(dispatch, error);
+    }
 };
 
 export const fetchPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
         dispatch({ type: FETCH_POSTS, payload: data });
-    } catch (error) { }
+    } catch (error) {
+        handleError(dispatch, error);
+    }
 };
 
 export const fetchComments = () => async (dispatch) => {
     try {
         const { data } = await api.fetchComments();
         dispatch({ type: FETCH_COMMENTS, payload: data });
-    } catch (error) { }
+    } catch (error) {
+        handleError(dispatch, error);
+    }
 };
